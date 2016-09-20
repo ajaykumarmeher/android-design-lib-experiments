2a3c3fc2a8545198dda9063bb0d8f04eda1fd0d7
akshay.kumar <akshay.kumar@olacabs.com>
2016-09-20 21:40:25 +0530

    added dialogefragment

---
+++ 1-Base/src/main/java/com.example.android.materialdesigncodelab/Listeners/TickDetailListener.java
@@ -0,0 +1,9 @@
+package com.example.android.materialdesigncodelab.Listeners;
+
+/**
+ * Created by akshay.kumar on 18/09/16.
+ */
+
+public interface TickDetailListener {
+  void onTickDetailClicked(int position, String tittle, String desc);
+}
