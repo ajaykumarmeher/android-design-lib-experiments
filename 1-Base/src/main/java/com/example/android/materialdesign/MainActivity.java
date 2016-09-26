fb26daea27e6a0f6a22bf6db6b1c850e874c4959
akshay.kumar <akshay.kumar@olacabs.com>
2016-09-26 09:44:11 +0530

    - Moved fab to Main Activity

--- 1-Base/src/main/java/com/example/android/materialdesign/MainActivity.java
+++ 1-Base/src/main/java/com/example/android/materialdesign/MainActivity.java
@@ -17,9 +17,11 @@
 package com.example.android.materialdesign;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -29,9 +31,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 import com.example.android.materialdesign.fragments.MainFragment;
 import com.example.android.materialdesign.ui.MainScreenHandler;
 import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements MainScreenHandler {
     private DrawerLayout mDrawerLayout;
@@ -40,6 +47,13 @@ public class MainActivity extends AppCompatActivity implements MainScreenHandler
     private TabLayout mTabs;
     private AppBarLayout mAppBarLayout;
     private FloatingActionButton mFab;
+
+    private FloatingActionMenu menuRed;
+    private List<FloatingActionMenu> menus = new ArrayList<>();
+    private FloatingActionButton fab1;
+    private FloatingActionButton fab2;
+    private FloatingActionButton fab3;
+    private Handler mUiHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,7 +94,75 @@ public class MainActivity extends AppCompatActivity implements MainScreenHandler
                     }
                 });
 
+        menuRed = (FloatingActionMenu) findViewById(R.id.menu_red);
+        final FloatingActionButton programFab1 = new FloatingActionButton(this);
+        programFab1.setButtonSize(FloatingActionButton.SIZE_MINI);
+        programFab1.setLabelText("Testing");
+        programFab1.setImageResource(R.drawable.ic_edit);
+        menuRed.addMenuButton(programFab1);
+
+
+
+        programFab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                programFab1.setLabelColors(ContextCompat.getColor(getApplicationContext(), R.color.grey),
+                    ContextCompat.getColor(getApplicationContext(), R.color.light_grey),
+                    ContextCompat.getColor(getApplicationContext(), R.color.white_transparent));
+                programFab1.setLabelTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+            }
+        });
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+        fab1.setEnabled(false);
+        menuRed.setClosedOnTouchOutside(true);
+        menuRed.hideMenuButton(false);
+
+        menus.add(menuRed);
+
+        fab1.setOnClickListener(clickListener);
+        fab2.setOnClickListener(clickListener);
+        fab3.setOnClickListener(clickListener);
+
+        int delay = 400;
+        for (final FloatingActionMenu menu : menus) {
+            mUiHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    menu.showMenuButton(true);
+                }
+            }, delay);
+            delay += 150;
+        }
+
+        menuRed.setOnMenuButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (menuRed.isOpened()) {
+                    Toast.makeText(getApplicationContext(), menuRed.getMenuButtonLabelText(), Toast.LENGTH_SHORT).show();
+                }
+
+                menuRed.toggle(true);
+            }
+        });
     }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.fab1:
+                    break;
+                case R.id.fab2:
+                    fab2.setVisibility(View.GONE);
+                    break;
+                case R.id.fab3:
+                    fab2.setVisibility(View.VISIBLE);
+                    break;
+            }
+        }
+    };
 
     private void loadMainScreen() {
         MainFragment mainFragment = MainFragment.newInstance(this);
--- 1-Base/src/main/java/com/example/android/materialdesign/fragments/MainFragment.java
+++ 1-Base/src/main/java/com/example/android/materialdesign/fragments/MainFragment.java
@@ -2,22 +2,17 @@ package com.example.android.materialdesign.fragments;
 
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import com.example.android.materialdesign.R;
 import com.example.android.materialdesign.exception.ScreenHandlerNotFoundException;
 import com.example.android.materialdesign.ui.MainScreenHandler;
-import com.github.clans.fab.FloatingActionButton;
-import com.github.clans.fab.FloatingActionMenu;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,12 +23,6 @@ import java.util.List;
  */
 public class MainFragment extends Fragment {
 
-    private FloatingActionMenu menuRed;
-    private List<FloatingActionMenu> menus = new ArrayList<>();
-    private FloatingActionButton fab1;
-    private FloatingActionButton fab2;
-    private FloatingActionButton fab3;
-    private Handler mUiHandler = new Handler();
     private MainScreenHandler screenHandler;
 
     public MainFragment() {
@@ -74,32 +63,6 @@ public class MainFragment extends Fragment {
 
     private void initViews(View view) {
         screenHandler.getToolbar().setTitle("Dashboard Screen");
-
-        menuRed = (FloatingActionMenu) view.findViewById(R.id.menu_red);
-        final FloatingActionButton programFab1 = new FloatingActionButton(getActivity());
-        programFab1.setButtonSize(FloatingActionButton.SIZE_MINI);
-        programFab1.setLabelText("Testing");
-        programFab1.setImageResource(R.drawable.ic_edit);
-        menuRed.addMenuButton(programFab1);
-
-
-
-        programFab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                programFab1.setLabelColors(ContextCompat.getColor(getActivity(), R.color.grey),
-                    ContextCompat.getColor(getActivity(), R.color.light_grey),
-                    ContextCompat.getColor(getActivity(), R.color.white_transparent));
-                programFab1.setLabelTextColor(ContextCompat.getColor(getActivity(), R.color.black));
-            }
-        });
-        fab1 = (FloatingActionButton) view.findViewById(R.id.fab1);
-        fab2 = (FloatingActionButton) view.findViewById(R.id.fab2);
-        fab3 = (FloatingActionButton) view.findViewById(R.id.fab3);
-        fab1.setEnabled(false);
-        menuRed.setClosedOnTouchOutside(true);
-        menuRed.hideMenuButton(false);
-
         // Setting ViewPager for each Tabs
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
         setupViewPager(viewPager);
@@ -109,51 +72,9 @@ public class MainFragment extends Fragment {
 
     @Override public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        menus.add(menuRed);
 
-        fab1.setOnClickListener(clickListener);
-        fab2.setOnClickListener(clickListener);
-        fab3.setOnClickListener(clickListener);
-
-        int delay = 400;
-        for (final FloatingActionMenu menu : menus) {
-            mUiHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    menu.showMenuButton(true);
-                }
-            }, delay);
-            delay += 150;
-        }
-
-        menuRed.setOnMenuButtonClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (menuRed.isOpened()) {
-                    Toast.makeText(getActivity(), menuRed.getMenuButtonLabelText(), Toast.LENGTH_SHORT).show();
-                }
-
-                menuRed.toggle(true);
-            }
-        });
     }
 
-
-    private View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.fab1:
-                    break;
-                case R.id.fab2:
-                    fab2.setVisibility(View.GONE);
-                    break;
-                case R.id.fab3:
-                    fab2.setVisibility(View.VISIBLE);
-                    break;
-            }
-        }
-    };
     // Add Fragments to Tabs
     private void setupViewPager(ViewPager viewPager) {
         MainFragment.Adapter adapter = new MainFragment.Adapter(getActivity().getSupportFragmentManager());
